24470827ab7244c023ff26fb7d758a5147126e93
Roshan Acharya <roshanacharya9808@gmail.com>
2025-12-22 23:01:27 +0545

    Port

--- Api/main.py
+++ Api/main.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 from pipeline.predict import predict_url
 import pandas as pd
 from urllib.parse import * 
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -54,4 +55,5 @@ def predict():
 
 # Run Server
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
